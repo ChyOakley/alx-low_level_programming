de46405b4cd4ed6d5e297b2688be4a974232d615
ChyOakley <chiomaameh1@gmail.com>
2022-12-14 00:19:35 -0800

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,16 +2,13 @@
 
 /**
  * print_alphabet - print the alphabet in lowercase from a-z.
- * Return: Always 0.
  */
-
-void print_alphabet(void);
+void print_alphabet(void)
 {
 	char ch;
 
 	for (ch = 'a'; ch <= 'z'; ch++)
-	{
 		_putchar(ch);
-	}
+
 	_putchar('\n');
 }
