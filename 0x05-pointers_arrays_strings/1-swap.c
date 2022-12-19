89e04cb86efd8e56bb41575845c4ed0ca0c14973
ChyOakley <chiomaameh1@gmail.com>
2022-12-19 12:10:43 -0800

    1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,19 @@
+es (15 sloc)  222 Bytes
+#include "main.h"
+
+/**
+ * swap_int - check description
+ * Description: swaps the values of two integers
+ * @a:input
+ * @b:input
+ * Return: 0
+ */
+
+void swap_int(int *a, int *b)
+{
+int temp;
+
+temp = *a;
+*a = *b;
+*b = temp;
+}
