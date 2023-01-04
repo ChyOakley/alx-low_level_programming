dc3dc32e9662b9330101ca2b988b8474a600848c
ChyOakley <chiomaameh1@gmail.com>
2023-01-03 16:15:59 -0800

    tasks

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -7,6 +7,8 @@
  * void prototypes()
  */
 
+#include <stdio.h>
+#include <stdlib.h>
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
