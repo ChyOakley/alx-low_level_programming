643e9807b4294a8f802ce18349d4b096e625fe97
ChyOakley <chiomaameh1@gmail.com>
2023-01-06 16:07:20 -0800

    Task

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - check description
+ * Description:print number of arguments passed into it
+ * @argc:input
+ * @argv:input
+ * Return:0
+ */
+
+int main(int argc, char __attribute__((__unused__)) *argv[])
+{
+printf("%d\n", argc - 1);
+return (0);
+}
