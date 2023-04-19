73470f701ee009b479f0f7586e690d094cc70239
ChyOakley <chiomaameh1@gmail.com>
2023-04-19 14:40:32 -0700

    Tasks

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,10 +1,11 @@
 #include "hash_tables.h"
 
 /**
- * key_index - It gives the index of a key
- * @key: It is the key of key/value pair
- * @size: It is the size of array of the hash table
- * Return: It is the index where key/value pair should be stored in array of hash table
+ * key_index - get index for key given
+ * @key: key (string) to hash
+ * @size: size of hash table
+ *
+ * Return: index of 'key' using djb2
  */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -3,7 +3,7 @@
 /**
  * hash_table_delete - deletes a hash table
  *
- * @ht: The pointer to the hash table
+ * @ht: pointer to the hash table
  * Return: no return
  */
 void hash_table_delete(hash_table_t *ht)
